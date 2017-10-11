5d2319c3a9cfe892991738dea1d1f484b7b98e32
Marcio Willian <willian_mw.cc@hotmail.com>
2017-10-11 16:33:09 -0300

    Criação dos modulos da view CadastroPedido

---
+++ src/main/java/com/mwcc/pedidovenda/controller/CadastroPedidosBean.java
@@ -0,0 +1,24 @@
+package com.mwcc.pedidovenda.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class CadastroPedidosBean {
+
+	private List<Integer> itens;
+	
+	public CadastroPedidosBean() {
+		itens = new ArrayList<Integer>();
+		itens.add(1);
+		
+	}
+
+	public List<Integer> getItens() {
+		return itens;
+	}
+}
