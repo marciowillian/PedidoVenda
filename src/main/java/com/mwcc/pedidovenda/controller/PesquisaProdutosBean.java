71cd69c11220d9f2579637174a8078d3dbc79719
Marcio Willian <willian_mw.cc@hotmail.com>
2017-10-11 14:33:28 -0300

    Criação de tela de PesquisaProduto

---
+++ src/main/java/com/mwcc/pedidovenda/controller/PesquisaProdutosBean.java
@@ -0,0 +1,28 @@
+package com.mwcc.pedidovenda.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class PesquisaProdutosBean{
+	
+	private List<Integer> produtosFiltrados;
+	
+	public PesquisaProdutosBean() {
+		produtosFiltrados = new ArrayList<>();
+		for(int i = 0; i < 50; i++) {
+			produtosFiltrados.add(i);
+		}
+		
+		//System.out.println(produtosFiltrados);
+	}
+
+	public List<Integer> getProdutosFiltrados() {
+		return produtosFiltrados;
+	}
+}
